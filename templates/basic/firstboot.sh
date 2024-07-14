617b5f0b875e9d31097e117eff54e8deda2ee0ec
Dave Eales <david.eales@outlook.com>
2024-07-14 16:53:17 +0000

    x

---
+++ templates/basic/firstboot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Your firstboot actions here
+
+# Example actions:
+echo "Running actions for first boot..."
+
+# Add your commands here
+ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+
+# Remove @reboot entry from /etc/crontab
+echo "Removing @reboot entry from /etc/crontab..."
+sudo sed -i '/@reboot/d' /etc/crontab
+
+# Delete this script file
+echo "Deleting firstboot.sh script..."
+sudo rm -f "$0"
